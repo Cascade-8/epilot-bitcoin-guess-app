297b0dab6ccf208928266670ff7a94f0bcfbf578
Cascade-8 <nico.schwarz@t-online.de>
2025-08-06 16:37:13 +0200

    updated next.config and removed unused standalone option

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  trailingSlash: true,
-  output: 'standalone',
+  trailingSlash: true
 }
 
 export default nextConfig
